4925a291c5f5105e8f42b11eff7d8ea5b16ccc26
Mayhesta Gilang Maul <mayhestagilangm@gmail.com>
2024-06-29 14:03:10 +0700

    uploud image

--- src/parts/HeroTeam.js
+++ src/parts/HeroTeam.js
@@ -11,10 +11,11 @@ export default function HeroTeam() {
       <Fade bottom>
         <div className="w-full sm:w-1/2 flex flex-col px-5 mb-5 sm:mb-0 sm:px-12 sm:mt-6 lg:mt-6 xl:mt-20">
           <h1 className="text-6xl text-theme-blue font-bold leading-tight mb-5">
-            Our Team
+            Tim Kami
           </h1>
           <p className="font-light text-xl text-gray-400 leading-relaxed">
-            Our team is full of passionate people and ready to make your dream software come true.
+            Tim kami penuh dengan orang-orang yang bersemangat dan siap
+            mewujudkan perangkat lunak impian Anda.
           </p>
         </div>
         <div className="w-full sm:w-1/2 sm:pr-12">
